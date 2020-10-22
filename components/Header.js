b323c2f958d9387996f227b8df9e953e839f3720
William Andree <williamandree@gmail.com>
2020-10-21 19:16:32 -0500

    experimenting with typography

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,9 @@
 export default function Header() {
   return (
     <section className="sectionHeader">
-      <p className="header">
-        Will Andrée A wonderful serenity has taken possession of my entire soul,
-        like these sweet mornings of spring which I enjoy with my whole heart.
-      </p>
+      <div className="header">
+        <div>willandree.xyz</div> <div> </div>
+      </div>
     </section>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,49 @@ export default function Home() {
         <Info />
         <hr />
         <Contact />
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+          <defs>
+            <filter
+              x="0%"
+              y="0%"
+              width="100%"
+              height="100%"
+              id="grain"
+              filterUnits="objectBoundingBox"
+              color-interpolation-filters="sRGB"
+            >
+              <feTurbulence
+                type="fractalNoise"
+                result="f1"
+                stitchTiles="noStitch"
+                baseFrequency="0.6"
+                numOctaves="1"
+                seed="0"
+              />
+              <feColorMatrix
+                type="matrix"
+                values="0.96     0     0     0     0    0     0.53     0     0     0    0     0     0.48     0     0     0     0     0     1     0 "
+              />
+              <feColorMatrix type="luminanceToAlpha" in="f2" result="f3" />
+              <feComposite
+                in="SourceGraphic"
+                in2="f3"
+                result="f4"
+                operator="in"
+              />
+              <feColorMatrix
+                type="matrix"
+                values="1 0 0 0 0.22      0 1 0 0 0.22      0 0 1 0 0.22      0 0 0 1 0"
+                in="f4"
+                result="f5"
+              />
+              <feMerge>
+                <feMergeNode in="SourceGraphic" />
+                <feMergeNode in="f5" />
+              </feMerge>
+            </filter>
+          </defs>
+        </svg>
       </main>
     </div>
   );
