60c72967ee87b99a081339cee0126bdb949b9f00
William Andree <williamandree@gmail.com>
2020-11-03 23:21:03 -0600

    trying some landing designs out

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,14 @@
 export default function Header() {
   return (
-    <section className="sectionHeader">
-      <div className="header">{/* <div>Will Andrée</div>  */}</div>
+    <section className="header">
+      <div className="headerContainer">
+        <div className="headerBody">
+          <div>developer</div>
+          <div>designer</div>
+        </div>
+        <div className="monogram">WA</div>
+        <img src="/thistle.png" />
+      </div>
     </section>
   );
 }
