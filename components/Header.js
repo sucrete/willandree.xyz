272eb5e30a8ca9c256bc85774708c7d0a0c59ab2
William Andree <williamandree@gmail.com>
2020-11-04 00:23:59 -0600

    trying some landing designs out

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,7 @@ export default function Header() {
   return (
     <section className="header">
       <div className="headerContainer">
-        <div className="headerBody">
-          <div>developer</div>
-          <div>designer</div>
-        </div>
-        <div className="monogram">WA</div>
+        <div className="monogram">Wm.</div>
         <img src="/thistle.png" />
       </div>
     </section>
