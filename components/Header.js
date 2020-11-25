1a346819a89783e49a7770dc36fff96965f5113d
William Andree <williamandree@gmail.com>
2020-11-25 17:26:33 -0600

    altering description dimensions

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,23 @@
 export default function Header() {
+  const descriptionItems = [
+    "i am a front-",
+    "end web developer",
+    "and graphic designer",
+    "trying to make",
+    "work that fux",
+  ];
+  const werk = descriptionItems.map((item, index) => (
+    <div className={`descItem${index}`}>{item}</div>
+  ));
   return (
     <section className="header">
       <div className="descriptionContainer">
-        <div className="iAmContainer rotate-wrap">
+        {/* <div className="iAmContainer rotate-wrap">
           <div class="box box-rotate box-rotate-gyro">
             <h1>i am</h1>
           </div>
-        </div>
-        <p className="description">
-          i am a front-end developer graphic designer
-        </p>
+        </div> */}
+        <p className="description">{werk}</p>
       </div>
     </section>
   );
