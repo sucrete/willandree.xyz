aa985df3166192772909f052ca6aea6709b12d0f
William Andree <williamandree@gmail.com>
2020-11-27 23:56:57 -0600

    added smaller capsules

--- components/ModalContent/MaxRosen.js
+++ components/ModalContent/MaxRosen.js
@@ -2,9 +2,12 @@ export default function MaxRosen() {
   return (
     <>
       <div className="projectDesignation">ii</div>
-
-      <div className="modalBody">
+      <div className="modalBody maxRosen">
         <h1 className="modalHeader">maxrosen.town</h1>
+        <div className="modalSubheading">
+          personal website for director/editor Max Rosen{" "}
+          <span className="lilCapsule">WIP</span>
+        </div>
       </div>
     </>
   );
