4ecc3d516272bcebb39b5d48773fb86b837e51fe
William Andree <williamandree@gmail.com>
2020-11-29 22:08:53 -0600

    added curved .navLine and entrance animation

--- components/Navigation.js
+++ components/Navigation.js
@@ -7,6 +7,9 @@ export default function Navigation() {
         <a>info</a>
         <a>contact</a> */}
       </span>
+      <svg viewBox="0 0 100 15" class="navLine">
+        <path d="M 3 8 Q 3 17 8 8 Q 12 1 15 8 Q 18 15 31 8 Q 39 4 74 8" />
+      </svg>
     </section>
   );
 }
