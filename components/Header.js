44755036c3667512f68fc670e32852052e70f297
William Andree <williamandree@gmail.com>
2021-01-14 01:55:37 -0600

    altering letter entrance effect on Header

--- components/Header.js
+++ components/Header.js
@@ -5,16 +5,31 @@ export default function Header() {
     "graphic designer",
     "hopeless neophile",
   ];
-  const l1 = "Front-end developer,";
+  const l1 = "Front-end Developer,";
+  const l2 = "Graphic Designer,";
+  const l3 = "& Hopeless Neophile";
   const line1 = l1.split("");
-  // const werk = descriptionItems.map((item, index) => (
-  //   <div className={`descItem${index}`}>{item}</div>
-  // ));
+  const line2 = l2.split("");
+  const line3 = l3.split("");
   const trail1 = useTrail(line1.length, {
-    config: { mass: 3, tension: 5300, friction: 190 },
+    config: { mass: 3, tension: 4300, friction: 200 },
     opacity: 1,
     x: 0,
     delay: 700,
+    from: { opacity: 0, x: 9 },
+  });
+  const trail2 = useTrail(line2.length, {
+    config: { mass: 3, tension: 4300, friction: 200 },
+    opacity: 1,
+    x: 0,
+    delay: 850,
+    from: { opacity: 0, x: 9 },
+  });
+  const trail3 = useTrail(line3.length, {
+    config: { mass: 3, tension: 4300, friction: 200 },
+    opacity: 1,
+    x: 0,
+    delay: 1000,
     from: { opacity: 0, x: 9 },
   });
   return (
@@ -38,30 +53,34 @@ export default function Header() {
               {line1[index]}
             </animated.span>
           ))}
-          {/* {trail1.map(({ x, ...rest }, index) => (
+        </div>
+        <div className="description line2Container">
+          {trail2.map(({ x, ...rest }, index) => (
             <animated.span
               className={`dItem descriptionItem${index}`}
-              key={descriptionItems[index]}
+              key={line2[index]}
               style={{
                 ...rest,
                 transform: x.interpolate((x) => `translate3d(0,${x}px,0)`),
               }}
             >
-              {descriptionItems[index]}
+              {line2[index]}
             </animated.span>
           ))}
-          {trail1.map(({ x, ...rest }, index) => (
+        </div>
+        <div className="description line3Container">
+          {trail3.map(({ x, ...rest }, index) => (
             <animated.span
               className={`dItem descriptionItem${index}`}
-              key={descriptionItems[index]}
+              key={line3[index]}
               style={{
                 ...rest,
                 transform: x.interpolate((x) => `translate3d(0,${x}px,0)`),
               }}
             >
-              {descriptionItems[index]}
+              {line3[index]}
             </animated.span>
-          ))} */}
+          ))}
         </div>
       </div>
     </section>
