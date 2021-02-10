8e5133c2bc9ad9ec6903b4bd3a4b781b2cbfd4c6
William Andree <williamandree@gmail.com>
2021-02-10 15:09:09 -0600

    updating after OS update

--- components/Info.js
+++ components/Info.js
@@ -1,9 +1,21 @@
 import { useInView } from "react-intersection-observer";
+import { useTrail, animated } from "react-spring";
+const schools = ["LaunchCode - Kansas City, MO", "Computer Science/Front-End Development", "Drury University - Springfield, MO", "Art History, focus on Architectural History", "Savannah College of Art and Design - Savannah, GA", "Art and Architecture Fundamentals"];
 export default function Info() {
   const { ref, inView } = useInView({
-    threshold: 0.25,
+    threshold: 0.5,
     triggerOnce: true,
   });
+  
+
+  const trail = useTrail(schools.length, {
+    config: { mass: 5, tension: 2000, friction: 250 },
+    opacity: inView ? 1 : 0,
+    delay: 400,
+    x: inView ? 0 : 50,
+    from: { opacity: 0, x: 50 },
+  });
+
   return (
     <section id="info" className="info">
       <span className={`title inView${inView}Title`}>Info</span>
@@ -11,7 +23,19 @@ export default function Info() {
         <div className={`education inView${inView}`}>
           <div className="educationHeader">Education</div>
           <div className="educationBody">
-            <div></div>
+          {trail.map(({ x, ...rest }, index) => (
+            <animated.div
+              className={`education education--${[index]}`}
+              key={schools[index]}
+              style={{
+                ...rest,
+                transform: x.interpolate((x) => `translate3d(0,${x}px,0)`),
+              }}
+             
+            >
+              {schools[index]}
+            </animated.div>
+          ))}
           </div>
         </div>
         <div className={`aboutMe inView${inView}AboutMe inView${inView}`}>
