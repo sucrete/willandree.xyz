b3494b0e85838cc5e22c07718bbe77b338583b9b
William Andree <williamandree@gmail.com>
2021-02-21 01:29:56 -0600

    holy shit dude...I successfully created a timed replacement of the header text with letteringjs style separation of individual letters surrounded with span tags...to create effects on the text later

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useTrail, animated } from "react-spring";
 
 export default function Header() {
@@ -7,6 +8,44 @@ export default function Header() {
     "Designer, Student of",
     "Novelty",
   ];
+
+  const arrayOfItems = [];
+
+  const splitMe = (_2array) => {
+    let thing2split = _2array.split("");
+    let splitArray = [];
+    thing2split.forEach((letter) => {
+      splitArray.push(`<span>${letter}</span>`);
+    });
+    return splitArray;
+  };
+
+  descriptionItems.forEach((descItem) => {
+    arrayOfItems.push(splitMe(descItem));
+  });
+
+  useEffect(() => {
+    const dItemBaby0 = document.getElementsByClassName("dItemBaby--0")[0];
+    const dItemBaby1 = document.getElementsByClassName("dItemBaby--1")[0];
+    const dItemBaby2 = document.getElementsByClassName("dItemBaby--2")[0];
+    const dItemBaby3 = document.getElementsByClassName("dItemBaby--3")[0];
+    const Baby0Join = "";
+
+    const swapBabies = (baby, which) => {
+      baby.innerHTML = "";
+      arrayOfItems[which].forEach((eachSpan) => {
+        baby.innerHTML += eachSpan;
+      });
+    };
+    const timer = setTimeout(() => {
+      console.log("This will run after 2.5 seconds!");
+      swapBabies(dItemBaby0, 0);
+      swapBabies(dItemBaby1, 1);
+      swapBabies(dItemBaby2, 2);
+      swapBabies(dItemBaby3, 3);
+    }, 2500);
+    return () => clearTimeout(timer);
+  });
   const trail = useTrail(descriptionItems.length, {
     config: { mass: 5, tension: 2000, friction: 250 },
     opacity: 1,
@@ -14,7 +53,7 @@ export default function Header() {
     height: 100,
     delay: 700,
     rotate: 0,
-    from: { opacity: 0, x: 20, height: 0, rotate: 3 },
+    from: { opacity: 0, x: 20, height: 0, rotate: 2 },
   });
   return (
     <section className="header">
@@ -30,6 +69,7 @@ export default function Header() {
               }}
             >
               <animated.div
+                className={`dItemBaby dItemBaby--${index}`}
                 style={{
                   height,
                   transform: rotate.interpolate(
