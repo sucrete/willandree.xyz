901e01743b4474c79f793c0735d7338f0f601390
William Andree <williamandree@gmail.com>
2021-03-03 13:28:48 -0600

    major styling changes to simplify in my mind using self-imposed design mandates

--- pages/index.js
+++ pages/index.js
@@ -26,43 +26,6 @@ export default function Home() {
         <Info />
         {/* <Contact/> */}
       </main>
-
-      <svg width="0" height="0">
-        <filter id="filter">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency=".01"
-            numOctaves="4"
-          />
-          <feDisplacementMap in="SourceGraphic" scale="180" />
-        </filter>
-
-        <filter id="filter2">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency=".01"
-            numOctaves="4"
-          />
-          <feDisplacementMap in="SourceGraphic" scale="220" />
-        </filter>
-
-        <filter id="filter2-2">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency=".015"
-            numOctaves="3"
-          />
-          <feDisplacementMap in="SourceGraphic" scale="300" />
-        </filter>
-        <filter id="filter3">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency=".009"
-            numOctaves="5"
-          />
-          <feDisplacementMap in="SourceGraphic" scale="280" />
-        </filter>
-      </svg>
     </div>
   );
 }
