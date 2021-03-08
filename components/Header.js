18344da502a0c55821848c3916eb9c7533619984
William Andree <williamandree@gmail.com>
2021-03-08 13:12:41 -0600

    got blotter object to work in my  react spring trails list

--- components/Header.js
+++ components/Header.js
@@ -25,19 +25,21 @@ export default function Header() {
     config: { mass: 5, tension: 2000, friction: 250 },
     opacity: 1,
     delay: 500,
-    from: { opacity: 0 },
+    x: 0,
+    from: { opacity: 0, x: 50 },
   });
 
   return (
     <section className="header">
       <div className="descriptionContainer trails-main">
         <div className="description line1Container">
-          {trail.map(({ ...rest }, index) => (
+          {trail.map(({ x, ...rest }, index) => (
             <animated.div
               className={`dItem dItem--${index} trails-text`}
               key={descriptionItems[index]}
               style={{
                 ...rest,
+                top: x.interpolate((x) => `${x}px`),
               }}
             >
               <animated.div className={`dItemBaby dItemBaby--${index}`}>
--- components/Navigation.js
+++ components/Navigation.js
@@ -1,15 +1,32 @@
+import { useTrail, animated } from "react-spring";
 export default function Navigation() {
+  const fullName = "Will Andrée";
+  const splitName = fullName.split(" ");
+  const trail = useTrail(splitName.length, {
+    config: { mass: 3, tension: 2500, friction: 200 },
+    opacity: 1,
+    x: 0,
+    delay: 700,
+    from: { opacity: 0, x: -20 },
+  });
   return (
     <section className="navigation">
       <span className="name">
-        <span className="wm">Will </span>
-        <span className="lastName">
-          <span>An</span>
-          <span className="andree dre">dré</span>
-          <span className="andree e">e</span>
-        </span>
+        {trail.map(({ x, ...rest }, index) => (
+          <animated.span
+            className={`nameItem nameItem--${index}`}
+            key={splitName[index]}
+            style={{
+              ...rest,
+              top: x.interpolate((x) => `${x}px`),
+            }}
+          >
+            {splitName[index]}
+            <span> </span>
+          </animated.span>
+        ))}
+        <span className="cone"></span>
       </span>
-      <span className="cone"></span>
     </section>
   );
 }
