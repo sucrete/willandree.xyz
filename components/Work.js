377653641fd37bf5f292f67610474ebcd323cebe
William Andree <williamandree@gmail.com>
2021-04-02 23:35:58 -0500

    re-styling some <414 and some <375 elements

--- components/Work.js
+++ components/Work.js
@@ -107,13 +107,14 @@ export default function Work() {
   const trail = useTrail(work.length, {
     config: { mass: 5, tension: 2200, friction: 200 },
     opacity: inView ? 1 : 0,
-    delay: 100,
+    delay: 700,
     x: inView ? 0 : 20,
     from: { opacity: 0, x: 20 },
   });
   const trail2 = useTrail(tittle.length, {
     config: { mass: 5, tension: 2200, friction: 200 },
     opacity: inView ? 1 : 0,
+    delay: 600,
     x: inView ? 0 : 30,
     from: { opacity: 0, x: 30 },
   });
