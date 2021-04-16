c3f0fd2f08882ad01bdd352fa76151fda1267707
William Andree <williamandree@gmail.com>
2021-04-16 01:15:12 -0500

    styling modals

--- components/ModalContent/DumpTRUCK.js
+++ components/ModalContent/DumpTRUCK.js
@@ -1,26 +1,109 @@
+function FauxPost() {
+  return (
+    <div
+      class="workContainer"
+      style={{
+        height: "185px",
+        width: "375px",
+        position: "relative !important",
+      }}
+    >
+      <span
+        class="artistName"
+        style={{
+          display: "inline",
+          position: "relative !important",
+          fontFamily: "Helvetica Neue, sans-serif",
+          fontSize: ".8em",
+          fontWeight: "500",
+          marginRight: "10px",
+          position: "relative",
+          paddingRight: "20px",
+        }}
+      >
+        FAUXreal
+        <img src="/piskel.svg" class="dumpHeart" />
+      </span>
+      <img
+        style={{ display: "inline" }}
+        src="https://lh3.googleusercontent.com/HEx4Zy4KbmPAsREWV1zRr2M3qHCzwHkVW5HjEUnLHbekgQ53paNwaAM8wosRJ3tTho2SiaF3Fik2JTtQDuzSrgK-LzLHghHYqYuO2oQCNpRQgGD8E7wdIzAfnlN7ybwzVmBsPWN05lSeB2AzHLe74gIC4xe735fLSZxYeqNu1cP2g983d6H8c9rZ5euS8MoTPScV6XkKRQ9Nl4ZYR0IUKf7mkq7llZ_a8CbKdTEi_z_40D2z-MazTwsToIKhBwjTuYUDNGCOMMk80XDTNgA5gp6RTDAlsUVonJM91mAfhd1pKPp-OQC0JnhwPh7pRPZjx_YaueU3J2NTORN6oEwER0a1BqZXTqPi_FBFjCpnBPYwNAI7TTxFLTsAM86Nlubyq0QslqGYzKFAq19I3pfbDkRJMidsbb0ZK9KstvOyaWwadwNVM2Hu7dDyJHGJ-EbfPrnfF-2-PQc0DQDmurc9ykQC-to12fgnjGYAYWtJrJyd3HIuD3ecRgJ76PjEE2km9l7EHZ7CRdE7SJcRQ3BJe5pD4WCnwMcajhW9B6bYys1vSuLn0EINlyrldY_DI4H_KJH_KLVgdt_sffJU8xOTGBILCmVXyRn1Or87XgVYlzRYsBBYZliICf_fdQg3aPVGu_HUSsHBCp3X6g_40mg2S8wzpom9l4WDcqr7zbE_E4DuNga8izs=w1440-h647-ft"
+        class="workComponent"
+      />
+    </div>
+  );
+}
+function RoboPost() {
+  return (
+    <div
+      class="workContainer"
+      style={{
+        position: "relative !important",
+        display: "inline",
+        width: "660px",
+      }}
+    >
+      <span
+        class="artistName"
+        style={{
+          display: "inline",
+          position: "relative !important",
+          fontFamily: "Helvetica Neue, sans-serif",
+          fontSize: ".8em",
+          fontWeight: "500",
+          marginRight: "10px",
+          position: "relative",
+          paddingRight: "20px",
+        }}
+      >
+        robocide
+        <img src="/piskel.svg" class="dumpHeart" />
+      </span>
+      <img
+        style={{ display: "inline", width: "auto", height: "320px" }}
+        src="https://lh3.googleusercontent.com/mqQl4MDlUJ0nqi6okr340vdImiLZseHPSpTE5e6zi2A0iNDU_JgxL7kDqM2484EQ85BJvaZuoSARa1LJBz5YlX1JTdaXcCuuqwJ91VnKwm97NsroT9jiiYhjCKt_6kVhHXK68vE7wRivUR3PSw_pz_CVOifC4QjycxuPrqCciLL3TWQHFiP82TDqHLuR4VGqM9MGKyS3BQvVMPpJEJBxiSIk8wEhKOVTqJ-4apdYJZo8xoHo1seacKg0G2ZBupzkb3NmdTPS2DyCYQLkhuEo_hL0VtwvuzK26Kcd4G9fpTXKSDjvAhYD5IAlnLIrSxY8d_-htwdn0vn7ZZ8UdbB95y1hnNzxMviDQlsvKdBK4YpVhcBzgcu5OSaUrvw_u9wjYprHPpaOInRzijbhD_aKcePbI3TK6qPGRS5KegB0D_CtA2hCklY4YK75j56Jdx-K5S5p6gf7zjiA0HMJSPwdlhKJdP6HlkCqcw4ROqYmp6S_Uuu5r1HQENZUY1VVfAwY-13pNVzg7IWpqvlzMsWX36fzp0esUTAuOCFCLtP8AZ9f3V1JZB9_WomZCkeFPHV7AznaJBIlyKc8U8msmnGDeGSlW_JbrU9myOWeXL3LiVurCUoihbUVDjjVZG6IpR6vEgd7MqwyrJY8D99uMhG3SVFHo4yYiDRIKE3-3xv7hfNiUj1LKzU=w1440-h550-ft"
+        class="workComponent"
+      />
+      <img
+        style={{ display: "inline", width: "250px", height: "auto" }}
+        src="https://lh3.googleusercontent.com/FJWEZzVLgIOV0GVG-ULqxzDFLu-K1DV_-V18fdbVcPuiniVhtPtlEyuoQNXGoKhFS-Ytkz5S6XVC_w5H4E82mRnTyBKrrS9s3r6hRfp8peDNXhKrfJ7Clsy8F62Ft8e6oMGnNiG-iu7p9KYSeKzYVVkk5ltiqck92PCSeMBg3EKKqDf_LKbbzMu-QV-qKy8GawKN9--EBNdFwHdcXAp6k3iCiaU_zDfFePecgEU1vNO7m6Q5PStIVEAtZQd9O3e2Jv6jivv7f4uRLGWqafw_VF8J927PeUUz6HqIM43HCWbs2MdfQylOm5JsVhYUd0J9CYCELo1dBBdhcUgAS2lFyAKGjJ3C6PYnP-49AX6iicltoUSg8jEvSEnKO4yKDcC7bFnPoKa1zSzcUtyOjzKKifiBHGnso3pPydRLTwzLip20VRDn_m2M2DuLfp1HVbB6kU_nDc4GBbpry_ooOalqD2apn48-wwIbvC2zkjOHhkkS3XQJ-cWhtnNThLrAVa9pVOWjZWY_d-nRIzinAI8kzC206KbhItw82OHoyhs4Vxb4_SiZhVWVGMWYCCQyzHRI7lpLHifjB-K888IbTE3pJ44XbN86rQFyKx5mLZUou-25MHDe75jZYTfUvxsA_6pP32pceYv2-U3GDTn91T-vQZqPVpiqWPY31kOzwuXU_yYfq7SXKjA=w1440-h550-ft"
+        class="workComponent"
+      />
+    </div>
+  );
+}
 const slides = [
-  { id: 0, url: "/dumpTRUCKlogo.svg" },
-  { id: 1, url: "/thistle.png" },
-  { id: 2, url: "/5532_DuckWaddle.gif" },
+  <img style={{ height: "60px", width: "auto" }} src="/dumpTRUCKlogo.svg" />,
+  <FauxPost />,
+  <RoboPost />,
 ];
 const imageSlides = slides.map((slide, index) => {
-  return (
-    <li className={`liImage liImage--${index}`}>
-      <img src={`${slide.url}`} alt="" />
-    </li>
-  );
+  return <li className={`liImage liImage--${index}`}>{slide}</li>;
 });
+
 export default function DumpTRUCK() {
   return (
     <>
       <div className="projectDesignation">2020</div>
-      <div className="modalBody dumpTruck">
+      <div className="modalBody dumpTRUCK">
         <h1 className="modalHeader">dumpTRUCK</h1>
+        <div className="modalSubheading">personal project</div>
         <div className="modalContent">
           <div className="modalImagesContainer">
             <ul>{imageSlides}</ul>
           </div>
-          <div className="modalDetails"></div>
+          <div className="modalDetails">
+            This site was created to provide a permanent home for a selection of
+            work from a central progenitor of "
+            <a
+              href="https://www.artspace.com/magazine/interviews_features/trend_report/post_internet_art-52138"
+              target="
+            _black"
+            >
+              post-internet art
+            </a>
+            ", an unconventional chatroom called dump.fm. Like a chatroom, a
+            text
+          </div>
         </div>
       </div>
     </>
