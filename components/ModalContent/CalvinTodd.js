61ce1b6977e70b0ca884abf2cd500c53369418b7
William Andree <williamandree@gmail.com>
2021-05-01 21:56:10 -0500

    adding images to Calvin Todd modal

--- components/ModalContent/CalvinTodd.js
+++ components/ModalContent/CalvinTodd.js
@@ -1,3 +1,12 @@
+const slides = [
+  <img src="/calcal.png" />,
+  <img src="/CalTittle.png" />,
+  <img src="/calvincircle.png" />,
+  <img src="/CalLeather.png" />,
+];
+const imageSlides = slides.map((slide, index) => {
+  return <li className={`liImage liImage--${index}`}>{slide}</li>;
+});
 export default function CalvinTodd() {
   return (
     <>
@@ -6,6 +15,17 @@ export default function CalvinTodd() {
       <div className="modalBody calvinTodd">
         <h1 className="modalHeader">calvintodd.com</h1>
         <div className="modalSubheading">personal website</div>
+        <div className="modalContent">
+          <div className="modalImagesContainer">
+            <ul>{imageSlides}</ul>
+          </div>
+          <div className="modalDetails">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque
+            impedit numquam praesentium recusandae blanditiis quia sequi,
+            repellat vel consectetur autem labore earum libero, quod minima
+            culpa optio sapiente expedita alias!
+          </div>
+        </div>
       </div>
     </>
   );
