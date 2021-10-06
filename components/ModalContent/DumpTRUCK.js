b04b538208e3c35a5de1dc654a853a2c07ab5ffc
William Andree <williamandree@gmail.com>
2021-10-06 01:24:09 -0500

    re-hosting images locally

--- components/ModalContent/DumpTRUCK.js
+++ components/ModalContent/DumpTRUCK.js
@@ -26,7 +26,7 @@ function FauxPost() {
       </span>
       <img
         style={{ display: "inline" }}
-        src="https://lh3.googleusercontent.com/HEx4Zy4KbmPAsREWV1zRr2M3qHCzwHkVW5HjEUnLHbekgQ53paNwaAM8wosRJ3tTho2SiaF3Fik2JTtQDuzSrgK-LzLHghHYqYuO2oQCNpRQgGD8E7wdIzAfnlN7ybwzVmBsPWN05lSeB2AzHLe74gIC4xe735fLSZxYeqNu1cP2g983d6H8c9rZ5euS8MoTPScV6XkKRQ9Nl4ZYR0IUKf7mkq7llZ_a8CbKdTEi_z_40D2z-MazTwsToIKhBwjTuYUDNGCOMMk80XDTNgA5gp6RTDAlsUVonJM91mAfhd1pKPp-OQC0JnhwPh7pRPZjx_YaueU3J2NTORN6oEwER0a1BqZXTqPi_FBFjCpnBPYwNAI7TTxFLTsAM86Nlubyq0QslqGYzKFAq19I3pfbDkRJMidsbb0ZK9KstvOyaWwadwNVM2Hu7dDyJHGJ-EbfPrnfF-2-PQc0DQDmurc9ykQC-to12fgnjGYAYWtJrJyd3HIuD3ecRgJ76PjEE2km9l7EHZ7CRdE7SJcRQ3BJe5pD4WCnwMcajhW9B6bYys1vSuLn0EINlyrldY_DI4H_KJH_KLVgdt_sffJU8xOTGBILCmVXyRn1Or87XgVYlzRYsBBYZliICf_fdQg3aPVGu_HUSsHBCp3X6g_40mg2S8wzpom9l4WDcqr7zbE_E4DuNga8izs=w1440-h647-ft"
+        src="/balloon.gif"
         className="workComponent"
       />
     </div>
@@ -60,12 +60,12 @@ function RoboPost() {
       </span>
       <img
         style={{ display: "inline", width: "auto", height: "320px" }}
-        src="https://lh3.googleusercontent.com/mqQl4MDlUJ0nqi6okr340vdImiLZseHPSpTE5e6zi2A0iNDU_JgxL7kDqM2484EQ85BJvaZuoSARa1LJBz5YlX1JTdaXcCuuqwJ91VnKwm97NsroT9jiiYhjCKt_6kVhHXK68vE7wRivUR3PSw_pz_CVOifC4QjycxuPrqCciLL3TWQHFiP82TDqHLuR4VGqM9MGKyS3BQvVMPpJEJBxiSIk8wEhKOVTqJ-4apdYJZo8xoHo1seacKg0G2ZBupzkb3NmdTPS2DyCYQLkhuEo_hL0VtwvuzK26Kcd4G9fpTXKSDjvAhYD5IAlnLIrSxY8d_-htwdn0vn7ZZ8UdbB95y1hnNzxMviDQlsvKdBK4YpVhcBzgcu5OSaUrvw_u9wjYprHPpaOInRzijbhD_aKcePbI3TK6qPGRS5KegB0D_CtA2hCklY4YK75j56Jdx-K5S5p6gf7zjiA0HMJSPwdlhKJdP6HlkCqcw4ROqYmp6S_Uuu5r1HQENZUY1VVfAwY-13pNVzg7IWpqvlzMsWX36fzp0esUTAuOCFCLtP8AZ9f3V1JZB9_WomZCkeFPHV7AznaJBIlyKc8U8msmnGDeGSlW_JbrU9myOWeXL3LiVurCUoihbUVDjjVZG6IpR6vEgd7MqwyrJY8D99uMhG3SVFHo4yYiDRIKE3-3xv7hfNiUj1LKzU=w1440-h550-ft"
+        src="/dog.png"
         className="workComponent"
       />
       <img
         style={{ display: "inline", width: "250px", height: "auto" }}
-        src="https://lh3.googleusercontent.com/FJWEZzVLgIOV0GVG-ULqxzDFLu-K1DV_-V18fdbVcPuiniVhtPtlEyuoQNXGoKhFS-Ytkz5S6XVC_w5H4E82mRnTyBKrrS9s3r6hRfp8peDNXhKrfJ7Clsy8F62Ft8e6oMGnNiG-iu7p9KYSeKzYVVkk5ltiqck92PCSeMBg3EKKqDf_LKbbzMu-QV-qKy8GawKN9--EBNdFwHdcXAp6k3iCiaU_zDfFePecgEU1vNO7m6Q5PStIVEAtZQd9O3e2Jv6jivv7f4uRLGWqafw_VF8J927PeUUz6HqIM43HCWbs2MdfQylOm5JsVhYUd0J9CYCELo1dBBdhcUgAS2lFyAKGjJ3C6PYnP-49AX6iicltoUSg8jEvSEnKO4yKDcC7bFnPoKa1zSzcUtyOjzKKifiBHGnso3pPydRLTwzLip20VRDn_m2M2DuLfp1HVbB6kU_nDc4GBbpry_ooOalqD2apn48-wwIbvC2zkjOHhkkS3XQJ-cWhtnNThLrAVa9pVOWjZWY_d-nRIzinAI8kzC206KbhItw82OHoyhs4Vxb4_SiZhVWVGMWYCCQyzHRI7lpLHifjB-K888IbTE3pJ44XbN86rQFyKx5mLZUou-25MHDe75jZYTfUvxsA_6pP32pceYv2-U3GDTn91T-vQZqPVpiqWPY31kOzwuXU_yYfq7SXKjA=w1440-h550-ft"
+        src="/fashion.png"
         className="workComponent"
       />
     </div>
@@ -77,21 +77,31 @@ const slides = [
   <RoboPost />,
 ];
 const imageSlides = slides.map((slide, index) => {
-  return <li key={`imagesforDumpTRUCK--${index}`} className={`liImage liImage--${index}`}>{slide}</li>;
+  return (
+    <li
+      key={`imagesforDumpTRUCK--${index}`}
+      className={`liImage liImage--${index}`}
+    >
+      {slide}
+    </li>
+  );
 });
 function ReactCapsule() {
   return (
-<span style={{ color: "white", background: "blue"}}className="lilCapsule">
-            React
-          </span>
-  )
+    <span style={{ color: "white", background: "blue" }} className="lilCapsule">
+      React
+    </span>
+  );
 }
 function NextCapsule() {
   return (
-<span style={{ color: "white", background: "black", marginLeft: "3px"}}className="lilCapsule">
-            Next.js
-          </span>
-  )
+    <span
+      style={{ color: "white", background: "black", marginLeft: "3px" }}
+      className="lilCapsule"
+    >
+      Next.js
+    </span>
+  );
 }
 export default function DumpTRUCK() {
   return (
@@ -99,14 +109,17 @@ export default function DumpTRUCK() {
       <div className="projectDesignation">2020</div>
       <div className="modalBody dumpTRUCK">
         <h1 className="modalHeader">dumpTRUCK</h1>
-        <div className="modalSubheading"><ReactCapsule/><NextCapsule/></div>
+        <div className="modalSubheading">
+          <ReactCapsule />
+          <NextCapsule />
+        </div>
         <div className="modalContent">
           <div className="modalImagesContainer">
             <ul>{imageSlides}</ul>
           </div>
           <h1 className="modalHeaderLower">dumpTRUCK</h1>
-          <div className="modalSubheadingLower" >
-            <ReactCapsule/> <NextCapsule/>
+          <div className="modalSubheadingLower">
+            <ReactCapsule /> <NextCapsule />
           </div>
           <div className="modalDetails">
             <p>
